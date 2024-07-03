57b22d25b3c250610c4147b134b24cbd4504f7e1
welielton <welielton.silva@hotmail.com>
2024-07-03 20:31:17 -0300

    Novo teste de criação de repositório

---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,44 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+    public void adicionarTarefa(String descricao){
+        //adiciona uma tarefa
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        //remove tarefas se repetidas
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+
+        }
+    }
+    public int obterNumeroTotalTarefas(){
+        //para saber a qtd de elementos da lista
+        return tarefaList.size();
+    }
+    public void obterDescricoesTarefas(){
+        //imprimindo a lista de tarefa
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        //Cria um objeto a partir da classe ListaTarefa
+        ListaTarefa vlistaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é: " + vlistaTarefa.obterNumeroTotalTarefas());
+    }
+}
